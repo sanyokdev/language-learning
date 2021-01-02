ee05502f1dd0cec81758acdd1760aba7ecd695fa
Alessandro Nikolaev <alessandronikolaev2001@gmail.com>
2021-01-02 19:54:53 +0000

    Added ability to request word definitions

--- Main/main.py
+++ Main/main.py
@@ -1,35 +1,96 @@
 import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "../Credentials/mykey.json"
 from google.cloud import texttospeech
+import requests
+import json
 
-# Instantiates a client
-client = texttospeech.TextToSpeechClient()
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./Credentials/mykey.json"
 
-# Set the text input to be synthesized
-synthesis_input = texttospeech.SynthesisInput(
-    text="A família de Luciano. Luciano tem oito anos e possui uma família que admira muito. Ele gosta de desenhar seus familiares quando está de férias."
-)
 
-# Build the voice request, select the language code ("en-US") and the ssml
-# voice gender ("female")
-voice = texttospeech.VoiceSelectionParams(
-    language_code='pt-PT',
-    name='pt-PT-Wavenet-C'
-)
+def convert_to_speech(word_id, input_text, output_path):
+    # Instantiates a client
+    client = texttospeech.TextToSpeechClient()
 
-# Select the type of audio file you want returned
-audio_config = texttospeech.AudioConfig(
-    audio_encoding=texttospeech.AudioEncoding.MP3
-)
+    # Set the text input to be synthesized
+    synthesis_input = texttospeech.SynthesisInput(
+        {
+            "text": str(input_text)
+        }
+    )
 
-# Perform the text-to-speech request on the text input with the selected
-# voice parameters and audio file type
-response = client.synthesize_speech(
-    input=synthesis_input, voice=voice, audio_config=audio_config
-)
+    # Build the voice request
+    # voice = texttospeech.VoiceSelectionParams(
+    #    language_code='pt-PT',
+    #    name='pt-PT-Wavenet-C'
+    # )
 
-# The response's audio_content is binary.
-with open('../output.mp3', 'wb') as out:
-    # Write the response to the output file.
-    out.write(response.audio_content)
-    print('Audio content written to file "output.mp3"')
+    voice = texttospeech.VoiceSelectionParams(
+        {
+            "language_code": "en-US",
+            "name": "en-US-Wavenet-D"
+        }
+    )
+
+    # Select the type of audio file you want returned
+    audio_config = texttospeech.AudioConfig(
+        {
+            "audio_encoding": texttospeech.AudioEncoding.MP3
+        }
+    )
+
+    # Perform the text-to-speech request on the text input with the selected voice parameters and audio file type
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
+
+    # The response's audio_content is binary.
+    with open("./Data/Output/" + str(output_path) + "/" + str(word_id) + ".mp3", "wb") as out:
+        # Write the response to the output file.
+        out.write(response.audio_content)
+        print("Audio content written to file" + str(word_id) + ".mp3")
+
+
+def get_word_definition(input_word, word_tense):
+    payload = {
+        "language": "pt",
+        "text": str(input_word),
+        "pos": str(word_tense)
+    }
+
+    url = ""
+    with open("./Credentials/dictapiUrl.txt", 'r') as outfile:
+        url = str(outfile.readline())
+
+    r = requests.get(url, params=payload)
+
+    results = json.loads(r.content)["results"]
+    senses = results[0]["senses"]
+
+    output_list = []
+
+    for val in senses:
+        definition = str(val.get("definition"))
+
+        if definition != "None":
+            output_list.append(val.get("definition"))
+
+    return output_list
+
+
+def main():
+    print(get_word_definition("ter", "verb"))
+
+    # print("Word ID?")
+    # word_id = str(input())
+
+    # print("Input text?")
+    # input_text = str(input())
+
+    # print("Output path?")
+    # output_path = str(input())
+
+    # convert_to_speech(word_id, input_text, output_path)
+    # print("Converted")
+
+
+if __name__ == "__main__":
+    main()
