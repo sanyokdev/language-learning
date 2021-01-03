ac0e309b0c02aa720f9acfd644eacf54bac118da
Alessandro Nikolaev <alessandronikolaev2001@gmail.com>
2021-01-03 19:31:37 +0000

    Make usablility more general

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import tts
 import definition
 
-# Verbs
-verb_words_file = "Data/Input/Words/Verbs.txt"
-verb_meaning_file = "Data/Input/Meaning/Verbs.txt"
-verb_sound_base_name = "VerbSND"
 
-tts.convert_file(verb_words_file, verb_sound_base_name, "Data/Output/Audio/Verbs")
+def create_audio_files(word_type):
+    words_file = "Data/Input/Words/" + str(word_type) + ".txt"
+    meaning_file = "Data/Input/Meaning/" + str(word_type) + ".txt"
+    sound_base_name = str(word_type).capitalize() + "SND"
 
-"""
-definition.convert_file_into_anki_import(
-    verb_words_file,
-    verb_meaning_file,
-    verb_sound_base_name,
-    "Data/Output/Anki/VerbsImport.txt",
-    "verb"
-)
-"""
+    tts.convert_file(words_file, sound_base_name, "Data/Output/Audio/" + str(word_type))
+
+
+def create_anki_file(word_type):
+    words_file = "Data/Input/Words/" + str(word_type) + ".txt"
+    meaning_file = "Data/Input/Meaning/" + str(word_type) + ".txt"
+    sound_base_name = str(word_type) + "SND"
+
+    definition.convert_file_into_anki_import(
+        words_file,
+        meaning_file,
+        sound_base_name,
+        "Data/Output/Anki/" + str(word_type) + "Import.txt",
+        str(word_type).lower()
+    )
+
+
+# create_audio_files("Noun")
+# create_anki_file("Noun")
