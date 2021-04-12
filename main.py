d0fb782a8fea1390807d79ae205acc70df61ada7
TheWinterDev <alessandronikolaev.business@gmail.com>
2021-04-12 23:51:28 +0100

    Fixed typo

--- main.py
+++ main.py
@@ -11,3 +11,4 @@ import re
 # jpHelper.create_anki_kana_deck("Hiragana", "Combination", "Symbol")
 # jpHelper.create_anki_kana_deck("Hiragana", "Main", "Stroke")
 # jpHelper.get_kana_sound_files("Combination")
+
