b987d777dad4404b35da64aedd67752082fdd8d1
TheWinterDev <alessandronikolaev.business@gmail.com>
2021-07-17 18:36:00 +0100

    Added basic RTK and JLPT support for Japanese

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # AnkiLanguageLearning
- 
+This is a project where I use Python in combination with Anki (Spaced repetition app) to learn and create flash cards for a language I want to learn/improve on "currently Japanese".
+
--- main.py
+++ main.py
@@ -1,14 +1,133 @@
-# Internal
-from WordManager import japaneseHelper as jpHelper
-
-# External
+import numpy
 import pandas as pd
+from bs4 import BeautifulSoup
 import requests
-import yaml
 import re
+from enum import Enum
+
+# region --- Enums
+
+class RTKVersion(Enum): # The "Remembering the Kanji" by James Heisig books
+    V1 = "https://hochanh.github.io/rtk/rtk1-v6/index.html"
+    V3 = "https://hochanh.github.io/rtk/rtk3-remain/index.html"
+
+class JLPTLvl(Enum): # The "Japanese-Language Proficiency Test" levels
+    N5 = 0
+    N4 = 1
+    N3 = 2
+    N2 = 3
+    N1 = 4
+
+class DataFilepath(Enum): # The filepath of the data files
+    RTK = "Japanese/Data/RTK/"
+    JLTP = "Japanese/Data/JLTP/"
 
 
-# jpHelper.create_anki_kana_deck("Hiragana", "Combination", "Symbol")
-# jpHelper.create_anki_kana_deck("Hiragana", "Main", "Stroke")
-# jpHelper.get_kana_sound_files("Combination")
+# endregion
+
+# region --- Definitions
+
+
+"""
+    Some Comment
+"""
+def get_element_text(element):
+    return str(re.search(r">(.*?)<", str(element)).group(1))
+
+
+"""
+    Some Comment
+"""
+def sort_kanji_to_specific_order(presetData, unsortedData):
+    dataID = []
+
+    for item in unsortedData:
+        indexID = -1
+        if item in presetData:
+            indexID = presetData.index(item)
+
+        dataID.append(indexID)
+
+    unsorted = numpy.array(unsortedData)
+    dataID = numpy.array(dataID)
+    inds = dataID.argsort()
+    return unsorted[inds].tolist()
+
+
+# region - RTK
+
+
+"""
+    Some Comment
+"""
+def get_kanji_rtk_preset(version: RTKVersion):
+    url = version.value
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    symbolElements = soup.find_all("a")[:-2]
+
+    symbolList = []
+    for element in symbolElements:
+        symbolList.append(get_element_text(element))
+
+    return symbolList
+
+
+"""
+    Some Comment
+"""
+def save_rtk_preset_as_csv(version: RTKVersion):
+    presetList = get_kanji_rtk_preset(version)
+
+    d = {'Kanji': presetList}
+    df = pd.DataFrame(data=d)
+
+    filepath = DataFilepath.RTK.value
+    df.to_csv(f"{filepath + str(version.name)}.csv", index=False, encoding="utf-8-sig")
+
+
+"""
+    Some Comment
+"""
+def load_rtk_as_list(version: RTKVersion):
+    filepath = DataFilepath.RTK.value
+    output = pd.read_csv(f"{filepath + str(version.name)}.csv").Kanji.to_list()
+
+    return output
+
+
+"""
+    Some Comment
+"""
+def load_all_rtk_as_list():
+    return load_rtk_as_list(RTKVersion.V1) + load_rtk_as_list(RTKVersion.V3)
+
+
+# endregion
+
+# region - JLTP
+
+"""
+    Some Comment
+"""
+def get_kanji_for_jlpt_level(level: JLPTLvl):
+    url = "https://www.kanshudo.com/collections/jlpt_kanji"
+    page = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    levelPanels = soup.find_all("div", {"class": "infopanel"})
+    symbolElements = levelPanels[level.value].find_all("a")
+
+    symbols = []
+    for element in symbolElements:
+        symbols.append(get_element_text(element))
+
+    return symbols
+
+
+# endregion
+
+
+# endregion
 
