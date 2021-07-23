2f1e95755079f863f1643c25c851c599c4367769
TheWinterDev <alessandronikolaev.business@gmail.com>
2021-07-24 00:23:41 +0100

    Added a custom rewrite/implementation of the WakaFont

---
+++ Helper/AutoClicker.ahk
@@ -0,0 +1,15 @@
+﻿#NoEnv  ; Recommended for performance and compatibility with future AutoHotkey releases.
+; #Warn  ; Enable warnings to assist with detecting common errors.
+SendMode Input  ; Recommended for new scripts due to its superior speed and reliability.
+SetWorkingDir %A_ScriptDir%  ; Ensures a consistent starting directory.
+
+#MaxThreadsPerHotKey, 2
+Toggle :=0
+F2::
+Toggle :=!Toggle
+While (Toggle=1)
+{
+	Click, Left
+	sleep, 50
+}
+return
--- main.py
+++ main.py
@@ -6,18 +6,17 @@ import requests
 from Japanese.Src import WaniKani
 
 
-
-
 """
     TODO: TURN THIS INTO A FUNCTION
 """
+
+
 session = WaniKani.get_session()
-
 gridData = WaniKani.get_grid_data(WaniKani.GridType.Radical, session)
-# with pd.option_context("display.max_rows", None, "display.max_columns", None):
-#     print(gridData)
+with pd.option_context("display.max_rows", None, "display.max_columns", None):
+    print(gridData)
 
-count = 0;
+count = 0
 for item in gridData.Symbol:
     test = re.search(r"[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)", item)
     if test is not None:
