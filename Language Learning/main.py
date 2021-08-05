28310c7aa990a00d92ab6908a52f80ef6b21a6d8
TheWinterDev <alessandronikolaev.business@gmail.com>
2021-08-05 17:54:15 +0100

    Added Kanji components scrapping for Vocabulary

--- Language Learning/main.py
+++ Language Learning/main.py
@@ -5,12 +5,12 @@ session = WaniKani.site.get_session()
 
 grid_type = WaniKani.data.GridType.Vocabulary
 item_list = [
-    CustomGridItem("Precision", "精度", "https://www.wanikani.com/vocabulary/%E7%B2%BE%E5%BA%A6"),
-    CustomGridItem("Existing", "既存", "https://www.wanikani.com/vocabulary/%E6%97%A2%E5%AD%98"),
-    CustomGridItem("Only", "唯一", "https://www.wanikani.com/vocabulary/%E5%94%AF%E4%B8%80"),
-    CustomGridItem("Cave", "洞穴", "https://www.wanikani.com/vocabulary/%E6%B4%9E%E7%A9%B4"),
-    CustomGridItem("Secretion", "分泌", "https://www.wanikani.com/vocabulary/%E5%88%86%E6%B3%8C"),
-    CustomGridItem("Diarrhea", "下痢", "https://www.wanikani.com/vocabulary/%E4%B8%8B%E7%97%A2")
+    CustomGridItem("Closet", "押入れ", "https://www.wanikani.com/vocabulary/%E6%8A%BC%E5%85%A5%E3%82%8C"),
+    CustomGridItem("Feudalism", "封建主義", "https://www.wanikani.com/vocabulary/%E5%B0%81%E5%BB%BA%E4%B8%BB%E7%BE%A9"),
+    CustomGridItem("Business", "営業", "https://www.wanikani.com/vocabulary/%E5%96%B6%E6%A5%AD"),
+    CustomGridItem("To Put In", "込める", "https://www.wanikani.com/vocabulary/%E8%BE%BC%E3%82%81%E3%82%8B"),
+    CustomGridItem("To Try", "試みる", "https://www.wanikani.com/vocabulary/%E8%A9%A6%E3%81%BF%E3%82%8B"),
+    CustomGridItem("Six", "六", "https://www.wanikani.com/vocabulary/%E5%85%AD")
 ]
 
 grid_data = WaniKani.data.get_custom_grid_data(item_list, grid_type)
