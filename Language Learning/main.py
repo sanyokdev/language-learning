2c938021b6fbc0e61c751e78befbdb274f115558
TheWinterDev <alessandronikolaev.business@gmail.com>
2021-08-08 00:04:11 +0100

    Refactored the main script to remove the unnecessary get_session call

--- Language Learning/main.py
+++ Language Learning/main.py
@@ -1,18 +1,12 @@
 import WaniKani
 from WaniKani.data import CustomGridItem
 
-session = WaniKani.site.get_session()
-
 grid_type = WaniKani.data.GridType.Radical
-"""
-item_list = [
-    CustomGridItem("", "", "")
-]
 
-grid_data = WaniKani.data.get_custom_grid_data(item_list, grid_type)
-"""
-grid_data = WaniKani.data.get_grid_data(grid_type, session)
+# item_list = [
+#     CustomGridItem("", "", "")
+# ]
 
-grid_item_data = WaniKani.data.get_grid_item_data(grid_data, session)
-grid_item_data.to_csv(f"-Output/WaniKani_{ str(grid_type.name) }_Data.csv", index=False)
-print(grid_item_data)
+# grid_data = WaniKani.data.get_custom_grid_data(item_list, grid_type)
+grid_data = WaniKani.data.get_grid_data(grid_type)
+grid_item_data = WaniKani.data.get_grid_item_data(grid_data, CHUNK_MODE=False, MAX_CHUNK_SIZE=1000)
