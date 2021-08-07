4a2b838306798e7896793b6e3a2aa78ca5a28525
TheWinterDev <alessandronikolaev.business@gmail.com>
2021-08-07 23:59:43 +0100

    Change the delay_offset to depend on the last item instead of the average, also changed progress text

--- Language Learning/Common/Stats.py
+++ Language Learning/Common/Stats.py
@@ -30,11 +30,10 @@ class TimeTracker:
         self.prev_times.append(self.end_time - self.start_time)
         self.avg_time_per_item = sum(self.prev_times) / len(self.prev_times)
 
-        self.delay_offset = 1.025 - self.avg_time_per_item
+        self.delay_offset = 1.025 - self.prev_times[len(self.prev_times) - 1]
         self.delay += self.delay_offset
 
-        if self.delay < 0:
-            self.delay = 0
+        self.delay = abs(self.delay)
 
     def print_progress(self):
         time_frac, time_whole = math.modf(round((self.avg_time_per_item * (self.total_items - self.progress)) / 60, 1))
@@ -43,13 +42,12 @@ class TimeTracker:
 
         if time_min >= 60:
             time_hour = int(time_min/60)
-            time_left_txt = f"Time Left: ({ time_hour }h:{ time_min - (60 * time_hour) }m:{ time_sec }s)"
+            time_left_txt = f"Time Left For Chunk: ({ time_hour }h:{ time_min - (60 * time_hour) }m:{ time_sec }s)"
         else:
-            time_left_txt = f"Time Left: ({ time_min }m:{ time_sec }s)"
+            time_left_txt = f"Time Left For Chunk: ({ time_min }m:{ time_sec }s)"
         prog_txt = f"Progress: ({ self.progress }/{ self.total_items })"
 
         print(f"{time_left_txt} | {prog_txt}")
-
 
     def print_stats(self):
         print(f"- RPM: { int(60 / self.avg_time_per_item) } | TPR: { round(self.avg_time_per_item, 2) }s")
